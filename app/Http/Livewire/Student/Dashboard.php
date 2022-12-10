c045b1c589b60959259f4829b5b3a087e06fc014
andarutr <andarutr@gmail.com>
2022-12-10 20:00:56 +0700

    Query Builder to Eloquent ORM [Student]

--- app/Http/Livewire/Student/Dashboard.php
+++ app/Http/Livewire/Student/Dashboard.php
@@ -3,16 +3,16 @@
 namespace App\Http\Livewire\Student;
 
 use Auth;
+use App\Models\Invoice;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        $invoice = \DB::table('invoice')
-                        ->where('id_user', Auth::user()->id)
-                        ->limit(5)
-                        ->get();
+        $invoice = Invoice::where('id_user', Auth::user()->id)
+                            ->limit(5)
+                            ->get();
 
         return view('livewire.student.dashboard', compact('invoice'));
     }
--- app/Http/Livewire/Student/Invoice/InvoiceTable.php
+++ app/Http/Livewire/Student/Invoice/InvoiceTable.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Student\Invoice;
 
 use Auth;
 use Livewire\Component;
+use App\Models\Invoice;
 use Livewire\WithPagination;
 
 class InvoiceTable extends Component
@@ -16,10 +17,9 @@ class InvoiceTable extends Component
 
     public function render()
     {
-        $invoice = \DB::table('invoice')
-                        ->join('users','invoice.id_user','=','users.id')
-                        ->where('id', Auth::user()->id)
-                        ->paginate($this->paginate);
+        $invoice = Invoice::where('id', Auth::user()->id)
+                            ->join('users','users.id','=','invoice.id_user')
+                            ->paginate($this->paginate);
 
         return view('livewire.student.invoice.invoice-table', compact('invoice'));
     }
